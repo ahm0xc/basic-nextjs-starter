992239b134ea608e7a9b7681c5ef932bd51caf78
ahm0xc <codingwebsa@gmail.com>
2025-03-05 22:33:26 +0600

    update next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  devIndicators: {
+    appIsrStatus: false,
+  },
+};
 
 export default config;
